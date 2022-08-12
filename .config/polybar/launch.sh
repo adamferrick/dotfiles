4dc12a07a72d9cf990c47d9e1c10a0c595ed1336
adamferrick <adameferrick@gmail.com>
2022-08-12 00:24:37 -0700

    status bar on multiple displays

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,4 +8,12 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload example &
+    done
+else
+    polybar --reload example &
+fi
+
 echo "Polybar launched..."
