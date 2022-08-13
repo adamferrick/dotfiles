40b8343d3236387b21f7b903b4b10c213b89c008
adamferrick <adameferrick@gmail.com>
2022-08-13 02:36:02 -0700

    one bar per screen

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,11 +2,6 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
-
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
