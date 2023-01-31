fdbad8409eeba54142ccbf9edf6916a9c83773a4
adamferrick <adameferrick@gmail.com>
2023-01-31 01:18:26 -0800

    script to assign stylus to a monitor

---
+++ .config/rofi/bin/stylus-monitor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+desktop="Full Desktop"
+displays="$(xrandr | awk '/ connected / {print $1}')"
+
+stylus="Wacom Intuos Pro M Pen stylus"
+
+selection=$(echo -e "$desktop\n$displays" | rofi -dmenu -i -p "Map $stylus to: ")
+
+if [[ "$selection" == "$desktop" ]]
+then
+    xsetwacom set "$stylus" ResetArea
+    xsetwacom set "$stylus" MapToOutput 'desktop'
+elif ! [[ $selection == "" ]]
+then
+    xsetwacom set "$stylus" Area 0 0 44800 25200
+    xsetwacom set "$stylus" MapToOutput $selection
+fi
