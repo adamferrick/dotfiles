cdb15c81a916a0865db341c889b52d2dcb08259b
adamferrick <adameferrick@gmail.com>
2023-04-28 01:48:29 -0700

    open grep matches in neovim

---
+++ .config/fish/functions/nvg.fish
@@ -0,0 +1,14 @@
+function nvg
+    set match $(
+        ag --nobreak --noheading -i $argv |
+        fzf -0 -1 |
+        awk -F: '{print $1, $2}'
+    )
+
+    set path $(echo $match | cut --delimiter=' ' -f1)
+    set line $(echo $match | cut --delimiter=' ' -f2)
+
+    if test -e $path
+        nvim "$path" +"$line"
+    end
+end
