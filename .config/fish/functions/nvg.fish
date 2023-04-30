c6d82f9a9cfe38ed99f380b2376d23b35dbd2070
adamferrick <adameferrick@gmail.com>
2023-04-29 23:35:50 -0700

    added preview window in grep open

--- .config/fish/functions/nvg.fish
+++ .config/fish/functions/nvg.fish
@@ -1,15 +1,19 @@
-# fuzzy grep open
+# fuzzy grep open with ag
 function nvg
-    set match $(
-        ag --nobreak --noheading -i $argv |
-        fzf -0 -1 |
-        awk -F: '{print $1, $2}'
-    )
-
-    set path $(echo $match | cut --delimiter=' ' -f1)
-    set line $(echo $match | cut --delimiter=' ' -f2)
-
-    if test -e $path
-        nvim "$path" +"$line"
-    end
+    rm -f /tmp/ag-fzf-{r,f}
+    set AG_PREFIX ag --nobreak --noheading --silent -i
+    $AG_PREFIX $argv |
+        fzf --ansi \
+            --color "hl:-1:underline,hl+:-1:underline:reverse" \
+            --disabled --query "$argv" \
+            --bind "change:reload:sleep 0.1; $AG_PREFIX {q} || true" \
+            --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/ag-fzf-r; cat /tmp/ag-fzf-f)" \
+            --bind "ctrl-r:unbind(ctrl-g)+change-prompt(1. grep> )+disable-search+reload($AG_PREFIX {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/ag-fzf-f; cat /tmp/ag-fzf-r)" \
+            --bind "start:unbind(ctrl-g)" \
+            --prompt '1. grep> ' \
+            --delimiter : \
+            --header '╱ CTRL-R (grep mode) ╱ CTRL-F (fzf mode) ╱' \
+            --preview 'bat --color=always {1} --highlight-line {2}' \
+            --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+            --bind 'enter:become(nvim {1} +{2})'
 end
