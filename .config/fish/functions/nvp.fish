ab45073382562eb99f2aa994785c37d4ab8c087d
adamferrick <adameferrick@gmail.com>
2023-04-30 01:33:09 -0700

    make commands similar to official example

--- .config/fish/functions/nvp.fish
+++ .config/fish/functions/nvp.fish
@@ -1,9 +1,19 @@
 # fuzzy path name open
 function nvp
-    set file $(
-        find $argv -type f |
+    rm -f /tmp/find-fzf-{r,f}
+    set FIND_INLINE 'find {q} -type f'
+    find $argv -type f |
         fzf \
-            --preview-window="50%" \
-            --preview="bat --color=always {1}"
-    ) && nvim $(echo "$file")
+            --color "hl:-1:underline,hl+:-1:underline:reverse" \
+            --disabled --query "$argv" \
+            --bind "change:reload:sleep 0.1; $FIND_INLINE || true" \
+            --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/find-fzf-r; cat /tmp/find-fzf-f)" \
+            --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. find> )+disable-search+reload($FIND_INLINE || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/find-fzf-f; cat /tmp/find-fzf-r)" \
+            --bind "start:unbind(ctrl-r)" \
+            --prompt '1. find> ' \
+            --delimiter : \
+            --header '╱ CTRL-R (find mode) ╱ CTRL-F (fzf mode) ╱' \
+            --preview 'bat --color=always {1}' \
+            --preview-window 'up,60%,border-bottom' \
+            --bind 'enter:become(nvim {1})'
 end
--- .config/fish/functions/nvt.fish
+++ .config/fish/functions/nvt.fish
@@ -1,12 +1,11 @@
 # fuzzy tags open
 function nvt
     [ -e tags ] &&
-    set line $(
-        awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
-        fzf \
-            --nth=1,2 \
-            --with-nth=2 \
-            --preview-window="50%" \
-            --preview="bat --color=always --line-range \$(echo {4} | tr -d \";\\\"\"): {3}"
-    ) && nvim $(echo "$line" | cut -f3) -c "silent tag $(echo "$line" | cut -f2)"
+    awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
+    string replace -r ';"$' '' |
+    fzf \
+        --with-nth=2 \
+        --preview-window "up,60%,border-bottom,+{4}+3/3,~3" \
+        --preview "bat --color=always {3} --highlight-line {4}" \
+        --bind "enter:become(nvim {3} +{4})"
 end
