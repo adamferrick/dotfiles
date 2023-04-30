44b5bd39bf4361a9fd52d17f7496d43f1ba28fba
adamferrick <adameferrick@gmail.com>
2023-04-30 02:14:47 -0700

    simplified commands

--- .config/fish/functions/ng.fish
+++ .config/fish/functions/ng.fish
@@ -1,17 +1,10 @@
 # fuzzy grep open with ag
 function ng
-    rm -f /tmp/ag-fzf-{r,f}
     set AG_PREFIX ag --nobreak --noheading --silent -i
-    $AG_PREFIX $argv |
+    ag --nobreak --noheading --silent -i $argv |
         fzf --ansi \
             --disabled --query "$argv" \
-            --bind "change:reload:sleep 0.1; $AG_PREFIX {q} || true" \
-            --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/ag-fzf-r; cat /tmp/ag-fzf-f)" \
-            --bind "ctrl-r:unbind(ctrl-g)+change-prompt(1. grep> )+disable-search+reload($AG_PREFIX {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/ag-fzf-f; cat /tmp/ag-fzf-r)" \
-            --bind "start:unbind(ctrl-g)" \
-            --prompt '1. grep> ' \
             --delimiter : \
-            --header '╱ CTRL-R (grep mode) ╱ CTRL-F (fzf mode) ╱' \
             --layout "reverse" \
             --height '95%' \
             --preview 'bat --color=always {1} --highlight-line {2}' \
--- .config/fish/functions/np.fish
+++ .config/fish/functions/np.fish
@@ -1,17 +1,9 @@
 # fuzzy path name open
 function np
-    rm -f /tmp/find-fzf-{r,f}
-    set FIND_INLINE 'find {q} -type f'
     find $argv -type f |
         fzf \
             --disabled --query "$argv" \
-            --bind "change:reload:sleep 0.1; $FIND_INLINE || true" \
-            --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/find-fzf-r; cat /tmp/find-fzf-f)" \
-            --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. find> )+disable-search+reload($FIND_INLINE || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/find-fzf-f; cat /tmp/find-fzf-r)" \
-            --bind "start:unbind(ctrl-r)" \
-            --prompt '1. find> ' \
             --delimiter : \
-            --header '╱ CTRL-R (find mode) ╱ CTRL-F (fzf mode) ╱' \
             --layout "reverse" \
             --height '95%' \
             --preview 'bat --color=always {1}' \
