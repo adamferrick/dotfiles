884a2a57a71349150664402074c3e40711ee6aca
adamferrick <adameferrick@gmail.com>
2023-05-25 01:19:52 -0700

    display filename

--- .config/fish/functions/nt.fish
+++ .config/fish/functions/nt.fish
@@ -5,7 +5,8 @@ function nt
         awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
         string replace -r ';"$' '' |
         fzf \
-            --with-nth=2 \
+            --with-nth=2,3 \
+            --nth=1 \
             --layout "reverse" \
             --height '95%' \
             --preview-window "down,60%,border-top,+{4}+3/3,~3" \
