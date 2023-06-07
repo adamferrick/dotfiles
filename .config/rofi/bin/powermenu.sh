3a7591f8c2e802cf030da8155872972df0cf57f5
adamferrick <adameferrick@gmail.com>
2023-06-07 01:46:57 -0700

    use fzfmenu

--- .config/rofi/bin/powermenu.sh
+++ .config/rofi/bin/powermenu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-command="rofi -dmenu -matching fuzzy -sort-method fzf -sort -i"
-
 poweroff="Power Off"
 restart="Restart"
 logout="Log Out"
@@ -10,35 +8,35 @@ lock="Lock"
 
 options="$poweroff\n$restart\n$logout\n$suspend\n$lock"
 
-chosen="$(echo -e "$options" | $command -p '> ')"
+chosen="$(echo -e "$options" | fzfmenu)"
 
 case $chosen in
     $poweroff)
-        ans="$(echo -e 'No\nYes' | $command -p "$poweroff"': Are you sure? ')"
+        ans="$(echo -e 'No\nYes' | fzfmenu --header="$poweroff"': Are you sure? ')"
         if [[ "$ans" = "Yes" ]]; then
             systemctl poweroff
         fi
         ;;
     $restart)
-        ans="$(echo -e 'No\nYes' | $command -p "$restart"': Are you sure? ')"
+        ans="$(echo -e 'No\nYes' | fzfmenu --header="$restart"': Are you sure? ')"
         if [[ "$ans" = "Yes" ]]; then
             systemctl reboot
         fi
         ;;
     $logout)
-        ans="$(echo -e 'No\nYes' | $command -p "$logout"': Are you sure? ')"
+        ans="$(echo -e 'No\nYes' | fzfmenu --header="$logout"': Are you sure? ')"
         if [[ "$ans" = "Yes" ]]; then
             loginctl terminate-session $XDG_SESSION_ID
         fi
         ;;
     $suspend)
-        ans="$(echo -e 'No\nYes' | $command -p "$suspend"': Are you sure? ')"
+        ans="$(echo -e 'No\nYes' | fzfmenu --header="$suspend"': Are you sure? ')"
         if [[ "$ans" = "Yes" ]]; then
             systemctl suspend
         fi
         ;;
     $lock)
-        ans="$(echo -e 'No\nYes' | $command -p "$lock"': Are you sure? ')"
+        ans="$(echo -e 'No\nYes' | fzfmenu --header="$lock"': Are you sure? ')"
         if [[ "$ans" = "Yes" ]]; then
             ~/.config/i3/bin/lock.sh
         fi
