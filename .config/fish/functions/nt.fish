8c9d999a3d84ab4d0b958db7b9b1c1f0107a9b9f
adamferrick <adameferrick@gmail.com>
2023-06-19 01:45:02 -0700

    table formatting for nt finder

--- .config/fish/functions/nt.fish
+++ .config/fish/functions/nt.fish
@@ -2,11 +2,13 @@
 function nt
     [ -e tags ] &&
     set line $(
-        awk 'BEGIN { FS="\t" } !/^!/ {print $4"\t"$1"\t"$2"\t"$3"\t"$5}' tags |
-        string replace -r ';"\t' '\t' |
+        awk 'BEGIN { FS="\t" } !/^!/ {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' tags |
+        column -t |
+        string replace -r ';"' '' |
         fzf \
             --with-nth=1,2,3,4,5 \
-            --preview-window "down,60%,border-top,+{4}+3/3,~3" \
-            --preview "bat --color=always {3} --highlight-line {4}"
-    ) && nvim $(echo "$line" | cut -f3) -c "silent tag $(echo "$line" | cut -f2)"
+            --preview-window "down,60%,border-top,+{3}+3/3,~3" \
+            --preview "bat --color=always {2} --highlight-line {3}" |
+        string replace -a -r '  +' '\t'
+    ) && nvim $(echo "$line" | cut -f2) -c "silent tag $(echo "$line" | cut -f1)"
 end
