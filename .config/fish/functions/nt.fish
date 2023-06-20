a139fe5d264583531ffb9e0557d2a68ebc0f74e9
adamferrick <adameferrick@gmail.com>
2023-06-20 00:38:23 -0700

    color in list for nt finder

--- .config/fish/functions/nt.fish
+++ .config/fish/functions/nt.fish
@@ -2,11 +2,33 @@
 function nt
     [ -e tags ] &&
     set line $(
-        awk 'BEGIN { FS="\t" } !/^!/ {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' tags |
+        awk '
+            BEGIN { FS="\t"; OFS="" } !/^!/ {
+                switch ($4) {
+                    case "C":
+                        color = "\033[91m";
+                        break
+                    case "s":
+                        color = "\033[92m";
+                        break
+                    case "c":
+                        color = "\033[92m";
+                        break
+                    case "f":
+                        color = "\033[93m";
+                        break
+                    default:
+                        color = "\033[94m";
+                        break
+                }
+                normal = "\033[94m";
+                print color,$1,normal,"\t"$2"\t"$3"\t",color,$4"\t",normal,$5
+            }
+        ' tags |
         column -t |
         string replace -r ';"' '' |
         fzf \
-            --with-nth=1,2,3,4,5 \
+            --ansi \
             --preview-window "down,60%,border-top,+{3}+3/3,~3" \
             --preview "bat --color=always {2} --highlight-line {3}" |
         string replace -a -r '  +' '\t'
