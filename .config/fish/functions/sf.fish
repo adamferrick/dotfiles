6f42545579e3e0ab72027aea8d7c8a47c3d1ab28
adamferrick <adameferrick@gmail.com>
2023-07-16 23:01:38 -0700

    make sf work for the eraser too

--- .config/fish/functions/sf.fish
+++ .config/fish/functions/sf.fish
@@ -2,17 +2,21 @@
 
 set desktop "Full Desktop"
 
-set stylus "Wacom Intuos Pro M Pen stylus"
+set devices "Wacom Intuos Pro M Pen stylus" "Wacom Intuos Pro M Pen eraser"
 
 function sf
     set displays "$(xrandr | awk '/ connected / {print $1}')"
     set selection $(echo -e "$desktop\n$displays" | fzf --header "Map $stylus")
 
     if test "$selection" = "$desktop"
-        xsetwacom set "$stylus" ResetArea
-        xsetwacom set "$stylus" MapToOutput 'desktop'
+        for device in $devices
+            xsetwacom set "$device" ResetArea
+            xsetwacom set "$device" MapToOutput 'desktop'
+        end
     else if not test "$selection" = ""
-        xsetwacom set "$stylus" Area 0 0 44800 25200
-        xsetwacom set "$stylus" MapToOutput $selection
+        for device in $devices
+            xsetwacom set "$device" Area 0 0 44800 25200
+            xsetwacom set "$device" MapToOutput $selection
+        end
     end
 end
