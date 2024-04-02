f9ee17f1639564e89d9aa9140d1e4647df96cb43
adamferrick <adameferrick@gmail.com>
2024-04-02 00:46:00 -0700

    display bar on all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,12 +3,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload example &
-    done
-else
-    polybar --reload example &
-fi
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
 
 echo "Polybar launched..."
