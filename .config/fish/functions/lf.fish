e53eca0a86fb9260817fe6b3fc1849dd85f9f3f0
Adam Ferrick <adameferrick@gmail.com>
2024-12-20 00:49:33 -0800

    add i3-launch script

--- .config/fish/functions/lf.fish
+++ .config/fish/functions/lf.fish
@@ -2,6 +2,6 @@
 function lf
     set selected $(complete -C | fzf --header "Launch App" --with-nth 1)
     if not test "$selected" = ""
-        i3-msg -q "exec --no-startup-id $(echo $selected | cut -f1)"
+        commandline "i3-launch "$(echo $selected | cut -f1)""
     end
 end
