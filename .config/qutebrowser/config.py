a61d2b346478f0aeeb05458b0f6b709d7760d341
Adam Ferrick <adameferrick@gmail.com>
2026-06-27 12:24:55 -0700

    open hints in a new window instead of a new tab

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,8 +2,9 @@ config.load_autoconfig()
 
 c.content.blocking.method = 'both'
 
-c.colors.webpage.darkmode.enabled = True
-
 config.set('content.headers.user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36')
 
 c.window.title_format = "{private}{current_title}{title_sep}qutebrowser"
+c.colors.webpage.darkmode.enabled = True
+
+config.bind('F', 'hint all window')
